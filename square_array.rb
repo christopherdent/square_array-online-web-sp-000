07a1666ec5dc37a54639eb4423c25c1f62355d00
Christopher Dent <christopherdent01@gmail.com>
2019-07-21 00:30:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,14 @@
 
 
 def square_array(numbers)
+  
   new_array = []
+  
     numbers.each do |number|
-      new_array = number ** 2
       
-    end 
+      new_array << number ** 2
+          end 
+          
     new_array
 end 
 
