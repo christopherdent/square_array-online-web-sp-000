24bb910d4821f117b190c0456172fffc5231f32c
Christopher Dent <christopherdent01@gmail.com>
2019-07-21 00:27:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,9 @@ numbers= [1, 2, 3]
 
 def square_array(numbers)
     numbers.each do |number|
-      var = number ** 2
+      
       new_array = []
-      new_array << var
+      new_array << number ** 2
     end 
 end 
 
